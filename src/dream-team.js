5affc17e12c60c17007d021b436adc4f2ba97688
emarf <frame.toravich@gmail.com>
2020-09-29 00:14:06 +0300

    feat: implement hanoi-tower task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(members) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  firstLetterArray = [];
+  const main = (members) => {
+    members.map(item => {
+      if (typeof item === 'string') {
+        item = item.trim();
+        firstLetterArray += item[0].toUpperCase();
+      }
+    })
+    return firstLetterArray.split('').sort().join('');
+  }
+
+  return (Array.isArray(members) && members !== []) ? main(members) : false;
 };
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+
+
+  const infoObj = {};
+  const turnsSpeedInSec = turnsSpeed / 3600;
+  const requiredDisks = Math.pow(2, disksNumber) - 1;
+
+  infoObj.turns = requiredDisks;
+  infoObj.seconds = Math.floor(requiredDisks / turnsSpeedInSec);
+
+  return infoObj;
 };
