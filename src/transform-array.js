8f00efbe802377fb280c9357098fb989a723b5e1
emarf <frame.toravich@gmail.com>
2020-09-29 12:41:44 +0300

    feat: implement transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  // --discard-next excludes the next element of the array from the transformed array.
+  // --discard-prev excludes the previous element of the array from the transformed array.
+  // --double-next doubles the next element of the array in the transformed array.
+  // --double-prev doubles the previous element of the array in the transformed array.
+
+  const exceptionArray = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+
+  let transformArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    switch (true) {
+      case arr[i] === exceptionArray[0]:
+        i++;
+        break;
+      case arr[i] === exceptionArray[1]:
+        if (arr[i - 2] !== exceptionArray[0]) {
+          transformArray.pop();
+        }
+        break;
+      case arr[i] === exceptionArray[2]:
+        if (i !== arr.length - 1) {
+          transformArray.push(arr[i + 1]);
+          break;
+        }
+        break;
+      case arr[i] === exceptionArray[3]:
+        if (i > 0 && arr[i - 2] !== exceptionArray[0]) {
+          transformArray.push(arr[i - 1]);
+          break;
+        }
+        break;
+
+      default:
+        transformArray.push(arr[i]);
+    }
+  }
+  return transformArray;
+}
+
