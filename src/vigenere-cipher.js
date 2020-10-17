ee166a0dcb2f9fb2d07698cecdeb8e8d8c37b768
emarf <frame.toravich@gmail.com>
2020-10-17 15:11:32 +0300

    feat: implement vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type = true) {
+    this.type = type ? 'direct' : 'reverse';
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+
+  checkForUndefined(message, key) {
+    if (key === undefined || message === undefined) {
+      throw new Error();
+    }
+  }
+
+  encrypt(message, key) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encrypt = '';
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        let encryptLetterNumber = ((message[i].charCodeAt() + key[j % key.length].charCodeAt()) % this.alphabet.length) + 65;
+        encrypt += String.fromCharCode(encryptLetterNumber);
+        j++;
+      } else {
+        encrypt += message[i]
+      }
+    }
+    return this.type === 'reverse' ? encrypt.split('').reverse().join('') : encrypt;
+  }
+  decrypt(message, key) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encrypt = '';
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        let encryptLetterNumber = ((message[i].charCodeAt() - key[j % key.length].charCodeAt() + 104) % this.alphabet.length) + 65;
+        encrypt += String.fromCharCode(encryptLetterNumber);
+        j++;
+      } else {
+        encrypt += message[i]
+      }
+    }
+    return this.type === 'reverse' ? encrypt.split('').reverse().join('') : encrypt;
   }
 }
 
